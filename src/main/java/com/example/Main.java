4def7d4ca9d0cf0d9782496c851a2ba8d0fa2313
yashikagirdhar88 <yashikagirdhar88@gmail.com>
2025-05-27 06:36:03 +0000

    yashika

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("going to start the spring boot Application...");
+		System.out.println("going to start the yashika's spring boot Application ");
 		SpringApplication.run(Main.class, args);
 	}
 
